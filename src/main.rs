b52a7b2b415755788586dba9bd891a154f6674d4
hoangphuc <hoangphucnb97@gmail.com>
2023-08-24 16:11:49 +0700

    Add test for database

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpListener;
+use sqlx::{PgPool};
 use zero2prod::configuration::get_configurations;
 use zero2prod::startup::run;
 
@@ -6,7 +7,10 @@ use zero2prod::startup::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configurations().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
